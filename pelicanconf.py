b7322a42c04e038ef4712ed933c68d518f4f0407
Matthew Stickney <fiendishlinux@gmail.com>
2015-04-26 16:04:42 -0400

    Add pelican files.

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Matthew Stickney'
+SITENAME = u'FiddlyBits'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+THEME='pelican-octopress-theme'
+# pelican-octopress configuration
+GITHUB_USER='mtstickney'
+GITHUB_REPO_COUNT='50'
+GITHUB_SHOW_USER_LINK='true'
+GITHUB_SKIP_FORK='true'
+SITESUBTITLE='A technical blog about fiddly things.'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
